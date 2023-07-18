36d020e6ce6c5a38c02f6947c3e25c4a073173d9
Nahid1911 <nahid.raihan@gmail.com>
2023-07-18 16:55:26 +0600

    Create Table inside vet_clinic DB

--- schema.sql
+++ schema.sql
@@ -1,5 +1,10 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals (
-    name varchar(100)
+id INT,
+name VARCHAR(255),
+date_of_birth DATE,
+escape_attempts INT,
+neutered BOOLEAN,
+weight_kg DECIMAL
 );
