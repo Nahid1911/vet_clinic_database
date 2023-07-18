a5d96467dabdfb4aa326f9dd29060181288e47c4
Nahid1911 <nahid.raihan@gmail.com>
2023-07-18 21:47:13 +0600

    Make required changes as per 1st review

--- data.sql
+++ data.sql
@@ -1,7 +1,7 @@
 /* Populate database with sample data. */
 
-INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg)
-VALUES (1, 'Agumon', '2020-02-03', 0, true, 10.23),
-(2, 'Gabumon', '2018-11-15', 2, true, 8.00),
-(3, 'Pickachu', '2021-01-07', 1, false, 15.04),
-(4, 'Devimon', '2017-05-12', 5, true, 11.00);
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg)
+VALUES ('Agumon', '2020-02-03', 0, true, 10.23),
+('Gabumon', '2018-11-15', 2, true, 8.00),
+('Pickachu', '2021-01-07', 1, false, 15.04),
+('Devimon', '2017-05-12', 5, true, 11.00);
--- queries.sql
+++ queries.sql
@@ -1,25 +1,17 @@
 /*Queries that provide answers to the questions from all projects.*/
 
-vet_clinic=# SELECT * FROM animals
-vet_clinic-# WHERE name LIKE '%mon';
+SELECT * FROM animals WHERE name LIKE '%mon';
 
-vet_clinic=# SELECT * FROM animals
-vet_clinic-# WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-12-31';
+SELECT * FROM animals WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-12-31';
 
-vet_clinic=# SELECT * FROM animals
-vet_clinic-# WHERE neutered != false  AND escape_attempts < 3;
+SELECT * FROM animals WHERE neutered != false  AND escape_attempts < 3;
 
-vet_clinic=# SELECT date_of_birth FROM animals
-vet_clinic-# WHERE name = 'Agumon';
+SELECT date_of_birth FROM animals WHERE name = 'Agumon' OR name = 'Pikachu';
 
-vet_clinic=# SELECT name, escape_attempts FROM animals
-vet_clinic-# WHERE weight_kg > 10.00;
+SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
 
-vet_clinic=# SELECT * FROM animals
-vet_clinic-# WHERE neutered = true;
+SELECT * FROM animals WHERE neutered = true;
 
-vet_clinic=# SELECT * FROM animals
-vet_clinic-# WHERE name NOT LIKE 'Gabumon';
+SELECT * FROM animals WHERE name NOT LIKE 'Gabumon';
 
-vet_clinic=# SELECT * FROM animals
-vet_clinic-# WHERE weight_kg BETWEEN 10.4 AND 17.3;
+SELECT * FROM animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
