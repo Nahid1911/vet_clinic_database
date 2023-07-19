c3606403a35515767ff393c3a4274e3ad9118258
Nahid1911 <nahid.raihan@gmail.com>
2023-07-19 23:04:39 +0600

    Add queries to answer questions

--- queries.sql
+++ queries.sql
@@ -43,3 +43,16 @@ BEGIN;
  ROLLBACK TO sp1;
  UPDATE animals SET weight_kg = weight_kg * -1;
 COMMIT;
+
+
+SELECT COUNT(name) AS number_of_animals FROM animals;
+
+SELECT COUNT(name) AS tried_to_escape FROM animals WHERE escape_attempts > 0;
+
+SELECT AVG(weight_kg) AS average_weight FROM animals;
+
+SELECT neutered, SUM(escape_attempts) AS total_escape_attempts FROM animals  GROUP BY neutered ORDER BY total_escape_attempts DESC LIMIT 1;
+
+SELECT species, MIN(weight_kg), MAX(weight_kg) FROM animals GROUP BY species;
+
+SELECT species, AVG(escape_attempts) AS average_escape_attempts FROM animals WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-12-31' GROUP BY species;
