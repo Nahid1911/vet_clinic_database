23d7cfbb36664399c78e44a46fd0e218a30e41c4
Nahid1911 <nahid.raihan@gmail.com>
2023-07-19 10:00:55 +0600

    Additional data inserted into animals table

--- data.sql
+++ data.sql
@@ -4,4 +4,10 @@ INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg)
 VALUES ('Agumon', '2020-02-03', 0, true, 10.23),
 ('Gabumon', '2018-11-15', 2, true, 8.00),
 ('Pickachu', '2021-01-07', 1, false, 15.04),
-('Devimon', '2017-05-12', 5, true, 11.00);
+('Devimon', '2017-05-12', 5, true, 11.00),
+('Palantmon', '2021-11-15', 2, true, 5.70),
+('Squirtle', '1993-04-02', 3, false, 12.13),
+('Angemon', '2005-06-12', 1, true, 45.00),
+('Boarmon', '2005-06-07', 7, true, 20.40),
+('Blossom', '1998-10-13', 3, true, 17.00),
+('Ditto', '2022-05-14', 4, true, 17.00);
