1dcdf83adead3d9a95c8f42ca01714370c7e294f
Nahid1911 <nahid.raihan@gmail.com>
2023-07-20 08:14:18 +0600

    Update weight of the animals

--- data.sql
+++ data.sql
@@ -7,10 +7,10 @@ VALUES ('Agumon', '2020-02-03', 0, true, 10.23),
 ('Devimon', '2017-05-12', 5, true, 11.00);
 
 INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg)
-VALUES ('Charmander', '2020-02-08', 2, true, 5.70),
-('Palantmon', '2021-11-15', 2, true, 5.70),
-('Squirtle', '1993-04-02', 3, false, 12.13),
-('Angemon', '2005-06-12', 1, true, 45.00),
+VALUES ('Charmander', '2020-02-08', 0, true, -11.00),
+('Palantmon', '2021-11-15', 2, true, -5.70),
+('Squirtle', '1993-04-02', 3, false, -12.13),
+('Angemon', '2005-06-12', 1, true, -45.00),
 ('Boarmon', '2005-06-07', 7, true, 20.40),
 ('Blossom', '1998-10-13', 3, true, 17.00),
 ('Ditto', '2022-05-14', 4, true, 17.00);
