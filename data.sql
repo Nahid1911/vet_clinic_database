85a515bcbedfcb50527b0f42e584713007961a2e
Nahid1911 <nahid.raihan@gmail.com>
2023-07-25 15:00:53 +0600

    Check database performance

--- data.sql
+++ data.sql
@@ -86,3 +86,14 @@ VALUES(1, 1, '2020-05-24'),
 (10, 3, '2020-05-24'),
 (10, 1, '2021-01-11');
 
+----------------------------------Module-4---Week-2---Day-1---------------------
+
+-- Add an email column to your owners table
+ALTER TABLE owners ADD COLUMN email VARCHAR(120);
+
+
+-- This will add 3.594.280 visits considering you have 10 animals, 4 vets, and it will use around ~87.000 timestamps (~4min approx.)
+INSERT INTO visits (animals_id, vets_id, visit_date) SELECT * FROM (SELECT id FROM animals) animals_id, (SELECT id FROM vets) vets_id, generate_series('1980-01-01'::timestamp, '2021-01-01', '4 hours') visit_timestamp;
+
+-- This will add 2.500.000 owners with full_name = 'Owner <X>' and email = 'owner_<X>@email.com' (~2min approx.)
+insert into owners (full_name, email) select 'Owner' || generate_series(1,2500000), 'owner_' || generate_series(1,2500000) || '@mail.com';
